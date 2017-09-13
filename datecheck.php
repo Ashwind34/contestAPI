326b563554b97a4dffff4ea18544eabf31bc9f7f
Ashwind34 <ahester34@gmail.com>
2017-09-12 22:33:04 -0700

    Corrected $weekmarker

--- datecheck.php
+++ datecheck.php
@@ -8,27 +8,26 @@ $today = date("Ymd");
 if ($today < "20170907") {
 	$date = strtotime("20170907");
 } else {
-	$date = strtotime(date("Ymd"));
+	$date = strtotime(date("Y/m/d h:i:sa"));
 }
 
 // MANUAL DATE OVERRIDE IF NEEDED
-//$date = strtotime("09/10/2017 9:15:00");
+//$date = strtotime("09/10/2017 11:30:00");
 
 $season_start = strtotime("09/05/2017");
 
 //$weekmarker variable will return current NFL week value
 
-$weekmarker = ceil((($date - $season_start)/"604800"));
+$datetest = (($date - $season_start)/"604800");
 
-//set variable to identify previous week, if needed
+$weekmarker = ceil($datetest);//ceil((($date - $season_start)/"604800"));
 
-$last_weekmarker = '';
+$kickoff_marker = $weekmarker - (($date - $season_start)/"604800");
 
-if ($weekmarker = 1) {
-	$last_weekmarker = 1;
-} else {
+//set variable to identify previous week
+
 $last_weekmarker = $weekmarker - 1;
-}
+
 
 
 
