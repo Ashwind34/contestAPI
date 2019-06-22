112260ad4a9a6254757a59a4ec4728ec5e0d9726
Ashwind34 <ahester34@gmail.com>
2019-06-22 14:50:11 -0700

    re-factored leaderboard.php to inlcude output buffer

--- src/leaderboard.php
+++ src/leaderboard.php
@@ -1,24 +1,6 @@
-<!DOCTYPE html>
-<html>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- set page to eliminate data cache storage on reload -->
-<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-<meta http-equiv="Pragma" content="no-cache" />
-<meta http-equiv="Expires" content="0" />
-<body>
-<?php 
-
+<?php
 require_once('datecheck.php');
 require_once('pdo_connect.php');
-
-
-?>
-
-<h1 style=text-align:center; style=color:blue;>LEADERBOARD</h1>
-<p style=text-align:center;><a href="../index.php">Return to Home Page</a></p>
-
-</body>
-<?php 
 
 //query for leaderboard table
 
@@ -43,7 +25,8 @@ $query = $conn->prepare(
 						WHERE
 							week = '$last_weekmarker'
 						ORDER BY
-						total_score DESC, name");
+						total_score DESC, name"
+);
 			
 		
 $query->execute();
@@ -51,13 +34,15 @@ $query->execute();
 //create array - data to be displayed in weekly picks table below.
 
 $data=$query->fetchall(PDO::FETCH_ASSOC);
-  
+
 //Make sure	query array is not empty, then create html table with all entries
-  
+//Save html output to variable with ob_start(), then echo in html below
+
 if (count($data) > 0) {
-	
-		
-		echo '<table align="center" border="1" cellspacing="5" cellpadding="8">
+
+	ob_start();
+
+	echo '<table align="center" border="1" cellspacing="5" cellpadding="8">
 		
 		<tr><th align="center">Player</th>
 		<th align="center">Favorite Team</th>
@@ -65,29 +50,44 @@ if (count($data) > 0) {
 		<th align="center">Last Week</th>
 		</tr>';
 		
-	// foreach loop to list out each row in the array	
+	// foreach loop to list out each row in the array
 		
-		foreach ($data as $row) {
-			
-				
-			echo 
+	foreach ($data as $row) {
+		echo
 			'<tr><td align="center">' . $row['name'] . '</td>
 			<td align="center">' . $row['fav_team'] . '</td>
 			<td align="center">' . $row['total_score'] . '</td>
 			<td align="center">' . $row['week_score'] . '</td></tr>';
-						
-		} 
+	}
 		
-		echo  '</table>';
-		
-		
-	} else {
-			echo "query problem";
-	
-			
+	echo  '</table>';
 
+	$leaderboard = ob_get_clean();
 
-		}
+} else {
+	echo "query problem";
+}
+
 ?>
-</body>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- set page to eliminate data cache storage on reload -->
+		<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+		<meta http-equiv="Pragma" content="no-cache" />
+		<meta http-equiv="Expires" content="0" />
+		<!--<link rel="stylesheet" type="text/css" href="style.css">-->
+	</head>
+	<body>
+		<h1 style=text-align:center; style=color:blue;>LEADERBOARD</h1>
+		<p style=text-align:center;><a href="../index.php">Return to Home Page</a></p><br>
+	
+		<?php 
+		
+		echo $leaderboard;
+		
+		?>
+	</body>
 </html>
