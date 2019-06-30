a3e85006fc44ed4503738bff1a98bd80c73934e3
Ashwind34 <ahester34@gmail.com>
2019-06-30 14:34:33 -0700

    continue updating table styling

--- src/weekly_picks_table.php
+++ src/weekly_picks_table.php
@@ -5,32 +5,33 @@
 		<link rel="stylesheet" type="text/css" href="../css/style.css">
 	</head>
 	<body>
+		<div class="picksContainer">
 
-		<?php 
+			<?php 
 
-		session_start();
+			session_start();
 
-		require_once('datecheck.php');
-		require_once('pdo_connect.php');
-		require_once('picks_query.php');
+			require_once('datecheck.php');
+			require_once('pdo_connect.php');
+			require_once('picks_query.php');
 
-		//if we are before approx. 11:30 AM PST on Sunday, show logged-in player's picks only.  Else show all player's picks.
+			//if we are before approx. 11:30 AM PST on Sunday, show logged-in player's picks only.  Else show all player's picks.
 
-		if ($kickoff_marker > 0.22) {
-			echo '<p>Your Picks for Week ' . $weekmarker . '<p>
-					<p><a href="../index.php">Return to Home Page</a></p><br>';
+			if ($kickoff_marker > 0.22) {
+				echo '<div class="formTitle">Your Picks for <br>Week ' . $weekmarker . '</div>
+						<a class="formLink" href="../index.php">Return to Home Page</a>';
 
-			echo $player_picks_table;
+				echo $player_picks_table;
+				
+			} else {
+				echo '<div class="formTitle">All Picks for <br>Week ' . $weekmarker . '</div>
+						<a class ="formLink" href="../index.php">Return to Home Page</a>';
+				
+				echo $weekly_picks_table;
+				
+			}
 			
-		} else {
-			echo '<p>Player Picks for Week ' . $weekmarker . '</p>
-					<p><a href="../index.php">Return to Home Page</a></p><br>';
-			
-			echo $weekly_picks_table;
-			
-		}
-		
-		?>
-	
+			?>
+		</div>
 	</body>
 </html>
