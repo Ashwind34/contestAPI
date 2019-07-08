190cb1aefaecd01fe24255714e0fee09a7fd0598
Ashwind34 <ahester34@gmail.com>
2019-07-07 23:57:28 -0700

    removed fav team from register form

--- src/register.php
+++ src/register.php
@@ -17,17 +17,18 @@ $email_query->execute();
 
 $email_list = $email_query->fetchAll(PDO::FETCH_ASSOC);
 
-// query db to get list of all NFL teams 
-$team_query = $conn->prepare(
-							"SELECT home AS teamlist FROM regseason WHERE week='2'
-							UNION
-							SELECT away AS teamlist FROM regseason WHERE week='2'
-							ORDER BY teamlist ASC"
-							);
-	
-$team_query->execute();	
+// query db to get list of all NFL teams - CURRENTLY DISABLED
 
-$team_list = $team_query->fetchAll(PDO::FETCH_ASSOC);
+// $team_query = $conn->prepare(
+// 							"SELECT home AS teamlist FROM regseason WHERE week='2'
+// 							UNION
+// 							SELECT away AS teamlist FROM regseason WHERE week='2'
+// 							ORDER BY teamlist ASC"
+// 							);
+	
+// $team_query->execute();	
+
+// $team_list = $team_query->fetchAll(PDO::FETCH_ASSOC);
 
 //Check to make sure form is empty
 
@@ -39,7 +40,7 @@ if (!empty($_POST['register'])) {
         
         //check to make sure all fields completed
                 
-        if (!empty($_POST['userpass']) && !empty($_POST['email']) && !empty($_POST['team'])) {
+        if (!empty($_POST['userpass']) && !empty($_POST['email']) /*&& !empty($_POST['team'])*/) {
 
         //check to make sure user has correct PIN
 
@@ -54,8 +55,7 @@ if (!empty($_POST['register'])) {
             //Prepared Statement to update favorite team and password
             
                 $query = "UPDATE player_roster 
-						SET fav_team = :team,
-						password = :password
+						SET password = :password
 						WHERE email = :email";
             
                 $submit = $conn->prepare($query);
@@ -64,14 +64,14 @@ if (!empty($_POST['register'])) {
             
                 $submit->BindParam(':email', $_POST['email']);
                 $submit->BindParam(':password', password_hash($_POST['userpass'], PASSWORD_BCRYPT));
-                $submit->BindParam(':team', $_POST['team']);
+                // $submit->BindParam(':team', $_POST['team']);
             
                             
                 //Submit query to database
 
                 if ($submit->execute()) {
                     echo '<br><p>Player Updated Successfully</p>';
-                    echo '<br><p><a href="../home.php">Return to Home Page</a></p>';
+                    echo '<br><p><a href="./home.php">Return to Home Page</a></p>';
                     exit();
                 } else {
                     echo '<br><p>Problem with Registration.  Please try again.</p>';
@@ -120,20 +120,24 @@ if (!empty($_POST['register'])) {
 					?>  
 
 				</select>	
-				<label for="team">Favorite NFL Team</label>
+
+				<!--FAVORITE TEAM SELECT CURRENTLY DISABLED-->
+				
+				<!-- <label for="team">Favorite NFL Team</label>
 				<select class="regselect" name="team">
 					
 					<option value="">-Select-</option>
 					
 					<?php 
 					
-					foreach ($team_list as $k=>$v) {
-						echo '<option value='. $v['teamlist'] . '>' . $v['teamlist'] . '</option>';
-					}
+					// foreach ($team_list as $k=>$v) {
+					// 	echo '<option value='. $v['teamlist'] . '>' . $v['teamlist'] . '</option>';
+					// }
 							
 					?>
 					
-				</select>
+				</select> -->
+
 				<label for="userpass">Select Password</label>
 				<input type="password" name="userpass" id="userpass">
 				<label for="confirmpass">Confirm Password</label>
