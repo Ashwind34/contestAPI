2d2001b7f72d39278063034f278d4b49bdc67c7d
Ashwind34 <ahester34@gmail.com>
2019-07-30 16:45:19 -0700

    centerd content on desktop for player picks table (on submit)

--- src/player_picks_table.php
+++ src/player_picks_table.php
@@ -20,24 +20,26 @@ if(empty($_SESSION['player_id'])) {
         <link rel="stylesheet" type="text/css" href="../css/style.css">
     </head>
     <body class="blackBack">
-        <div class="playerpicksContainer">
-            <div class="pickSubmit">
-                <div>
-                    Your picks for
+    <div class="wrapper">
+            <div class="playerpicksContainer">
+                <div class="pickSubmit">
+                    <div>
+                        Your picks for
+                    </div>
+                    <div>
+                        week <?php echo $weekmarker; ?> are in!
+                    </div>
                 </div>
                 <div>
-                    week <?php echo $weekmarker; ?> are in!
+                    <?php echo $player_picks_table;?>
                 </div>
+                <div class="formLink">
+                    <a class="noMargin" href="../index.php">Return to Home Page</a>
+                </div>
+                <div>
+                    <img class="boPic" src="../css/img/TDbo.png" alt="Bo knows Tecmo Bowl">
+                </div>           
             </div>
-            <div>
-                <?php echo $player_picks_table;?>
-            </div>
-            <div class="formLink">
-                <a class="noMargin" href="../index.php">Return to Home Page</a>
-            </div>
-            <div>
-                <img class="boPic" src="../css/img/TDbo.png" alt="Bo knows Tecmo Bowl">
-            </div>           
         </div>
         <audio src="../css/audio/touchdown.mp3" id="page_audio"></audio>
         <script src="../audio.js"></script>
