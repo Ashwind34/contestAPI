ac813b4f77430b1c89cc9665a4970449b399ee3a
Ashwind34 <ahester34@gmail.com>
2019-08-02 20:26:07 -0700

    removed wrapper from weekly lines table

--- src/weekly_lines_table.php
+++ src/weekly_lines_table.php
@@ -20,21 +20,19 @@ if(empty($_SESSION['player_id'])) {
         <link rel="stylesheet" type="text/css" href="../css/style.css">
     </head>    
     <body>
-        <div class ="wrapper">
-            <div class ="schedContainer">            
-                <div class="formTitle">
-                    Week <?php echo $weekmarker ;?> Lines
-                </div>      
-                <div class='picksSubmit'>
-                    <?php echo $weekly_lines_table ?>
-                </div>
-                <div class='formLink'>
-                    <a href='picksinput.php'>Check your Picks</a>
-                    <a href="../index.php">Return to Home Page</a>
-                </div>
+        <div class ="schedContainer">            
+            <div class="formTitle">
+                Week <?php echo $weekmarker ;?> Lines
+            </div>      
+            <div class='picksSubmit'>
+                <?php echo $weekly_lines_table ?>
             </div>
-            <br>
-            <br>
+            <div class='formLink'>
+                <a href='picksinput.php'>Check your Picks</a>
+                <a href="../index.php">Return to Home Page</a>
+            </div>
         </div>
+        <br>
+        <br>
     </body>
 </html>
