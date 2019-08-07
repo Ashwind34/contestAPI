6220fc09fce8bfbe4f3719403cf811a0b768b46f
Ashwind34 <ahester34@gmail.com>
2019-08-07 14:45:46 -0700

    error handling not working. working to re-factor

--- src/pinupdate.php
+++ src/pinupdate.php
@@ -14,24 +14,31 @@ function PinUpdate($email) {
                     WHERE email = :email";
     $pin_change = $conn->prepare($pin_update);
     $pin_change->BindParam(':email', $email);
-    if(null !== ($pin_change->execute())) {
-        echo "<p>There was a problem with your PIN reset.  Please check your email and try again.</p>";
-        echo '<p><a href="pinresetfresh.php">Try Again</a></p><br>';
-        echo '<p><a href="home.php">Return to Home Page</a></p>';
-        exit();
-    }
+    $pin_change->execute();
+    
+    
+    // if(null !== ($pin_change->execute())) {
+    //     echo "<p>There was a problem with your PIN reset.  Please check your email and try again.</p>";
+    //     echo '<p><a href="pinresetfresh.php">Try Again</a></p><br>';
+    //     echo '<p><a href="home.php">Return to Home Page</a></p>';
+    //     exit();
+    // }
 
     //sql query to get info about user to populate email message
     $email_query = $conn->prepare("SELECT email, first_name, last_name FROM player_roster WHERE email = :email");
     $email_query->BindParam(':email', $email);
-    if($email_query->execute()){
-        $email_query_array = $email_query->fetch(PDO::FETCH_ASSOC);
-    } else {
-        echo "<p>There was a problem with your PIN reset.  Please check your email and try again.</p>";
-        echo '<p><a href="pinresetfresh.php">Try Again</a></p><br>';
-        echo '<p><a href="home.php">Return to Home Page</a></p>';
-        exit();
-    }
+    $email_query->execute();
+    $email_query_array = $email_query->fetch(PDO::FETCH_ASSOC);
+    
+    
+    // if($email_query->execute()){
+    //     $email_query_array = $email_query->fetch(PDO::FETCH_ASSOC);
+    // } else {
+    //     echo "<p>There was a problem with your PIN reset.  Please check your email and try again.</p>";
+    //     echo '<p><a href="pinresetfresh.php">Try Again</a></p><br>';
+    //     echo '<p><a href="home.php">Return to Home Page</a></p>';
+    //     exit();
+    // }
   
 
     //assign info from db to variables to use in email message
