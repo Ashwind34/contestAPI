4eb767a751dda52fba3da4e1bf7a3855df20e2eb
Ashwind34 <ahester34@gmail.com>
2019-08-07 01:39:07 -0700

    added param binding and error handling in pinupdate

--- src/pinupdate.php
+++ src/pinupdate.php
@@ -11,14 +11,28 @@ function PinUpdate($email) {
     //query to update PIN in db
     $pin_update =   "UPDATE player_roster 
                     SET pin = '$pin'
-                    WHERE email = '$email'";
+                    WHERE email = :email";
     $pin_change = $conn->prepare($pin_update);
-    $pin_change->execute();
+    $pin_change->BindParam(':email', $email);
+    if(null !== ($pin_change->execute())) {
+        echo "<p>There was a problem with your PIN reset.  Please check your email and try again.</p>";
+        echo '<p><a href="pinresetfresh.php">Try Again</a></p><br>';
+        echo '<p><a href="home.php">Return to Home Page</a></p>';
+        exit();
+    }
 
     //sql query to get info about user to populate email message
-    $email_query = $conn->prepare("SELECT email, first_name, last_name FROM player_roster WHERE email = '$email'");
-    $email_query->execute();
-    $email_query_array = $email_query->fetch(PDO::FETCH_ASSOC);
+    $email_query = $conn->prepare("SELECT email, first_name, last_name FROM player_roster WHERE email = :email");
+    $email_query->BindParam(':email', $email);
+    if($email_query->execute()){
+        $email_query_array = $email_query->fetch(PDO::FETCH_ASSOC);
+    } else {
+        echo "<p>There was a problem with your PIN reset.  Please check your email and try again.</p>";
+        echo '<p><a href="pinresetfresh.php">Try Again</a></p><br>';
+        echo '<p><a href="home.php">Return to Home Page</a></p>';
+        exit();
+    }
+  
 
     //assign info from db to variables to use in email message
     $recipientEmail = $email;
