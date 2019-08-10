22c32973805063afd43f0559610b93eecc55bd95
Ashwind34 <ahester34@gmail.com>
2019-08-09 21:47:28 -0700

    removed redundant code from leaderboard.php.  Added text for current week

--- src/leaderboard.php
+++ src/leaderboard.php
@@ -1,87 +1,6 @@
 <?php
-require_once('datecheck.php');
-require_once('pdo_connect.php');
 
-session_start();
-
-if(empty($_SESSION['player_id'])) {
-
-	header("Location: ./login.php");
-
-}
-
-//query for leaderboard table
-
-$query = $conn->prepare(
-						"SELECT
-							player_picks.player_id,
-							player_picks.week_score,
-							player_picks.week,
-							CONCAT(
-								player_roster.first_name,
-								' ',
-								player_roster.last_name
-							) AS name,
-							player_roster.total_score,
-							player_roster." . $qtrcol . " AS qtr
-						FROM
-							player_picks
-						INNER JOIN
-							player_roster
-						ON
-							player_picks.player_id = player_roster.player_id
-						WHERE
-							week = '$last_weekmarker'
-						ORDER BY
-						total_score DESC, name"
-);
-			
-		
-$query->execute();
-					
-//create array - data to be displayed in weekly picks table below.
-
-$data=$query->fetchall(PDO::FETCH_ASSOC);
-
-//Make sure	query array is not empty, then create html table with all entries
-//Save html output to variable with ob_start(), then echo in html below
-
-if (count($data) > 0) {
-
-	ob_start();
-
-	echo 
-	'<table>		
-		<tr>
-			<th>Player</th>			
-			<th>Total Score</th>
-			<th>Qtrly Score</th>
-			<th>Last Week</th>
-		</tr>';
-		
-
-		
-	// foreach loop to list out each row in the array
-		
-	foreach ($data as $row) {
-		echo
-		'<tr>
-			<td>' . $row['name'] . '</td>			
-			<td>' . $row['total_score'] . '</td>
-			<td>' . $row['qtr'] . '</td>
-			<td>' . $row['week_score'] . '</td>
-		</tr>';
-	}
-
-	echo  '</table>';
-
-	//save output table to variable
-
-	$leaderboard = ob_get_clean();
-
-} else {
-	echo "query problem";
-}
+require_once('leaderboardtable.php');
 
 ?>
 
@@ -98,7 +17,10 @@ if (count($data) > 0) {
 	<body class="blackBack">
 		<div class="boardContainer">
 			<div class='formTitle'>
-				LEADERBOARD
+				LEADERBOARD 
+			</div>
+			<div class='formTitle'>
+				WEEK <?php echo $weekmarker ?>
 			</div>
 			<div class="formLink">
 				<a class="noMargin" href="../index.php">Return to Home Page</a>
