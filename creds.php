338ad68b441500d4e1655c2ffd0cad4a24f89cf7
Andrew S Hester <ahester34@gmail.com>
2019-10-02 00:52:37 -0700

    Added blank credential files

---
+++ creds.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername = ""; // database server
+$username = ""; // database username
+$password = ""; // database password
+$dbname = ""; // database name
+
+?>
---
+++ email_conf.php
@@ -0,0 +1,8 @@
+<?php
+$fromAddress = "";  // delivering email address
+$fromName = ""; // name for delivering email address
+$emailServer = ""; //email server url
+$emailUser = ""; //email account name
+$emailPass = ""; //email password
+?>
+
